ebc0c3fdf050653cd4b9c972d1c40ddacbffcf9f
miketaco <mike.tacopino@gmail.com>
2013-08-05 22:09:20 -0400

    first commit test
    testing repository commit

---
+++ HelloWorld/src/com/taco/urlTester.java
@@ -0,0 +1,37 @@
+package com.taco;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class urlTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+		System.out.println("HELLO");
+//		try{
+//		URL oracle = new URL("http://www.oracle.com/");
+//
+//		URLConnection yc =
+//		    oracle.openConnection();
+//		BufferedReader in = new BufferedReader(
+//		                    new InputStreamReader(
+//		                    yc.getInputStream()));
+//		String inputLine;
+//
+//		while ((inputLine = in.readLine()) != null) 
+//		System.out.println(inputLine);
+//		in.close();
+//		
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
+		
+	}
+
+}
